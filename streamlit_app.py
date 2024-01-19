0a327ffbec9e629fc84324918d1264f0fb6496a7
cmsnowflaketest <157116607+cmsnowflaketest@users.noreply.github.com>
2024-01-19 15:55:08 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,11 +41,8 @@ try:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
     
-
 except URLError as e:
   streamlit.error()
-
-# don't run anything past here while we troubleshoot
 
 streamlit.header("The fruit load list contains:")
 #Snowflake-realted functions
@@ -61,5 +58,12 @@ if streamlit.button('Get Fruit Load List'):
   streamlit.dataframe(my_data_rows)
 
 
-#This will not work correctly, but jut go with it for now
+# Allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+    return "Thanks for adding " + new_fruit
+ 
+add_my_fruit = streamlit.text_input('What fruit would you like too add?')
+
 
